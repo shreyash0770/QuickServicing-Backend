fb0ce5c734c0d18efb1ef726e0fee250a59dfdbb
Shreyash <shreyashrabhang@gmail.com>
2022-04-01 19:38:02 +0530

    1 april

--- src/main/java/com/main/controller/VehicleController.java
+++ src/main/java/com/main/controller/VehicleController.java
@@ -3,6 +3,7 @@ package com.main.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.main.model.Vehicle;
 import com.main.repository.VehicleRepository;
 
+@CrossOrigin(origins = "http://localhost:3000" )
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class VehicleController {
 	
 	@Autowired
